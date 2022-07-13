6a438d9d7d1774df4563bfb5d9544d49037635bb
JulioCunh <fcezarf@gmail.com>
2022-07-13 20:44:45 -0300

    Configurando cliente do Axios

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
-
 export function App() {
   return (
     <>
       <Header />
-      <Dashboard
+      <Dashboard />
       <GlobalStyle />
     </>
   );
