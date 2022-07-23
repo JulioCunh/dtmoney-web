ca533168a56520b2bf2f07340b39fcceaf63474c
JulioCunh <fcezarf@gmail.com>
2022-07-23 11:38:38 -0300

    Criando botões de Tipo

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,8 @@
 import Modal from 'react-modal';
-import { Container } from '..//NewTransactionModal/styles';
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactcionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,6 +13,8 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+
   return (
     <Modal
       isOpen={isOpen}
@@ -17,11 +22,40 @@ export function NewTransactionModal({
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar" />
+      </button>
+
       <Container>
         <h1>Cadastrar transação</h1>
 
         <input placeholder="titulo" />
         <input type="number" placeholder="valor" />
+        <TransactcionTypeContainer>
+          <button
+            type="button"
+            onClick={() => {
+              setType('deposit');
+            }}
+          >
+            <img src={incomeImg} alt="Income" />
+            <span>Entrada</span>
+          </button>
+
+          <button
+            type="button"
+            onClick={() => {
+              setType('withdraw');
+            }}
+          >
+            <img src={outcomeImg} alt="Outcome" />
+            <span>Saída</span>
+          </button>
+        </TransactcionTypeContainer>
         <input placeholder="categoria" />
 
         <button type="submit">Cadastrar</button>
